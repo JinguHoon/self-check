a56ae832efb66c9450cf4d336c8bd8440f150e7b
Seung Hoon <tmdgns6475@naver.com>
2021-06-09 16:58:30 +0900

    exe변환 전용 코드 완성

--- selfcheck.py
+++ selfcheck.py
@@ -1,12 +1,30 @@
+#-*- coding: utf-8 -*-#
+from base64 import decode
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import os.path
 
+file = "index.txt"
+if os.path.isfile(file):
+    f = open("index.txt", "r+", encoding="utf-8")
+    name = f.readline()
+    yymmdd = f.readline()
+    pw = f.readline()
+
+else : 
+    f = open("index.txt", "w+", encoding="utf-8")
+    name = input("이름 : ")
+    yymmdd = input("생년월일 6자리 : ")
+    pw = input("비밀번호 4자리 : ")
+    f.write("%s\n" %name)
+    f.write("%s\n" %yymmdd)
+    f.write("%s\n" %pw)
+    f.close()
+
+print(name, yymmdd, pw)
 url = "https://hcs.eduro.go.kr/#/relogin"
 schoolname = "한국디지털미디어고등학교"
-name = ""
-yymmdd = ""
-pw = ""
 
 
 driver = webdriver.Chrome("chromedriver.exe")
@@ -29,14 +47,14 @@ link = driver.find_element_by_xpath("//*[@id='softBoardListLayer']/div[2]/div[1]
 link.click()
 driver.find_element_by_css_selector('.layerFullBtn').click()
 
+time.sleep(2)
 driver.find_element_by_css_selector('#user_name_input').send_keys(name)
 driver.find_element_by_css_selector('#birthday_input').send_keys(yymmdd)
-driver.find_element_by_css_selector('#btnConfirm').click()
+button = driver.find_element_by_css_selector('#btnConfirm').send_keys("\n")
 
-time.sleep(1)
+time.sleep(2)
 driver.find_element_by_xpath("//*[@id='WriteInfoForm']/table/tbody/tr/td/input").send_keys(pw)
 driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
-
 time.sleep(2)
 driver.find_element_by_xpath("/html/body/app-root/div/div[1]/div[2]/div/section[2]/div[2]/ul/li/a").send_keys("\n")
 
@@ -47,4 +65,5 @@ driver.find_element_by_xpath("//*[@id='survey_q3a1']").click()
 driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
 time.sleep(2)
 driver.close()
+exit()
 
