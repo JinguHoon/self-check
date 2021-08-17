4b0232fa19e5966ba0990471c829c72422d4dec5
Lee Seung Hoon <tmdgns6475@naver.com>
2021-08-17 15:25:43 +0900

    a

---
+++ hello.js
@@ -0,0 +1,17 @@
+let coven = 86;
+
+if(coven>=90) {
+	console.log('A등급');
+}
+else if(coven>=80) {
+	console.log('B등급');
+}
+else if(coven>=70) {
+	console.log('C등급');
+}
+else if(coven>=60) {
+	console.log('D등급');
+}
+else {
+	console.log('E등급');
+}
